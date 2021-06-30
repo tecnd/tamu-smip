278a679502acc4f899dc08fae8cd78866b28c672
Wang <kzwang@tamu.edu>
2021-06-30 17:36:07 -0500

    minor fix

--- strptime_fix.py
+++ strptime_fix.py
@@ -3,7 +3,6 @@ from datetime import datetime
 
 def strptime_fix(ts: str) -> datetime:
     '''Temporary fix for missing microsecond field in GraphQL response.'''
-    fmt = ''
     if len(ts) > 26:
         fmt = '%Y-%m-%dT%H:%M:%S.%f%z'
     elif len(ts) == 25:
