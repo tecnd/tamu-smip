a599c830c3c2db0fd5b2187f7c6ef931e14f113e
Wang <kzwang@tamu.edu>
2021-07-01 16:47:56 -0500

    Fixed sin plotter discontinuities

--- sin_plotter.py
+++ sin_plotter.py
@@ -6,42 +6,39 @@ import numpy as np
 import pandas as pd
 import requests
 
-from smip_io import ENDPOINT, MUTATION_ADDDATA, update_token
+from smip_io import ENDPOINT, MUTATION_ADDDATA, get_token
 
 
-def sin_plot(freq1: float, freq2: float) -> None:
-    token = ''
+def sin_plot(rate: int, freq1: float) -> None:
+    token = get_token("test", "smtamu_group", "parthdave", "parth1234")
     t = 0
     with requests.Session() as s:
+        now = datetime.now(timezone.utc)
         while True:
-            now = datetime.now(timezone.utc)
             future = now + timedelta(seconds=1)
-            token = update_token(
-                token, "test", "smtamu_group", "parthdave", "parth1234")
-            time_range = pd.date_range(now, future, periods=1024)
-            val_range = np.arange(t, t+1024, dtype=np.single)
-            val_range *= 2*np.pi/1024
-            val_range = np.sin(freq1 * val_range) + 0.5 * \
-                np.sin(freq2 * val_range)
+
+            time_range = pd.date_range(now, future, periods=rate)
+            val_range = np.arange(t, t+rate, dtype=np.single)
+            val_range *= 2*np.pi/rate
+            val_range = np.sin(freq1 * val_range)
             payload = [{'timestamp': ts.isoformat(), 'value': str(val), 'status': 0}
                        for ts, val in zip(time_range, val_range)]
-            r = s.post(ENDPOINT, json={
+            s.post(ENDPOINT, json={
                 "query": MUTATION_ADDDATA,
                 "variables": {
                     "id": 5356,
                     "entries": payload
                 }
             }, headers={"Authorization": f"Bearer {token}"})
-            logging.info(str(r.elapsed) + ' ' + str(r.json()))
-            t += 1024
-            freq2 += 10
-            if freq2 > 500:
-                freq2 = 0
-            time.sleep(
-                max((future - datetime.now(timezone.utc)).total_seconds(), 0))
+
+            t += rate
+            now = future
+            rest = future - datetime.now(timezone.utc)
+            time.sleep(max(rest.total_seconds(), 0))
+            logging.info('Sleeping for %s', rest)
 
 
 if __name__ == "__main__":
     logging.basicConfig(filename='sin_plotter.log',
                         format='%(asctime)s %(levelname)s %(message)s', filemode='w', level=logging.DEBUG)
-    sin_plot(100, 80)
+    sin_plot(8000, 20)
