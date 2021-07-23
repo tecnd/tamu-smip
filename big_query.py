8ceffdd83c40da1c947cce73ef70f07eed941b3c
Kerry Wang <kzwang@tamu.edu>
2021-07-23 18:55:01 -0500

    Migrated big_little to smip_io

--- big_query.py
+++ big_query.py
@@ -1,22 +1,15 @@
 from datetime import datetime
 from time import perf_counter
 
-import requests
+import smip_io
 
-from smip_io import ENDPOINT, QUERY_GETDATA, get_token
-
-token = get_token("test", "smtamu_group", "parthdave", "parth1234")
+token = smip_io.get_token("test", "smtamu_group", "parthdave", "parth1234")
 print(token[-6:])
 print(datetime.now(), 'Starting big download request')
 start_timer = perf_counter()
-r = requests.post(ENDPOINT, json={
-    'query': QUERY_GETDATA,
-    'variables': {
-        'endTime': '2021-07-01T21:22:51.984520+00:00',
-        'startTime': '2021-07-01T21:21:51.984520+00:00',
-        'ids': [5356]
-    }
-}, headers={'Authorization': f'Bearer {token}'})
+r = smip_io.get_data(end_time='2021-07-01T21:22:51.984520+00:00',
+                     start_time='2021-07-01T21:21:51.984520+00:00',
+                     ids=[5356], token=token)
 elapsed = perf_counter() - start_timer
 print(len(r.json()['data']['getRawHistoryDataWithSampling']))
 print(datetime.now(), f'Got {len(r.content)} bytes in {elapsed} seconds')
--- little_query.py
+++ little_query.py
@@ -5,9 +5,9 @@ from math import nan
 import requests
 import matplotlib.pyplot as plt
 
-from smip_io import ENDPOINT, QUERY_GETDATA, get_token
+import smip_io
 
-token = get_token("test", "smtamu_group", "parthdave", "parth1234")
+token = smip_io.get_token("test", "smtamu_group", "parthdave", "parth1234")
 print(token[-6:])
 elapsed_times = list()
 start_times = list()
@@ -18,14 +18,9 @@ try:
             print(start_ts, 'Starting little download request')
             start_timer = perf_counter()
             try:
-                r = s.post(ENDPOINT, json={
-                    'query': QUERY_GETDATA,
-                    'variables': {
-                        'endTime': '2021-07-01T21:22:53.000000+00:00',
-                        'startTime': '2021-07-01T21:22:52.000000+00:00',
-                        'ids': [5356]
-                    }
-                }, headers={'Authorization': f'Bearer {token}'})
+                r = smip_io.get_data(end_time='2021-07-01T21:22:53.000000+00:00',
+                                     start_time='2021-07-01T21:22:52.000000+00:00',
+                                     ids=[5356], token=token, session=s)
             except requests.exceptions.ConnectionError:
                 print(datetime.now(), 'Connection failed')
                 start_times.append(start_ts)
@@ -35,7 +30,8 @@ try:
             start_times.append(start_ts)
             elapsed_times.append(elapsed)
             print(len(r.json()['data']['getRawHistoryDataWithSampling']))
-            print(datetime.now(), f'Got {len(r.content)} bytes in {elapsed} seconds')
+            print(datetime.now(),
+                  f'Got {len(r.content)} bytes in {elapsed} seconds')
             sleep(1)
 except KeyboardInterrupt:
     plt.plot(start_times, elapsed_times, 'o-')
