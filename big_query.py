a9d5f2137f42bf34ab38e4790172ee981141d708
Kerry Wang <kzwang@tamu.edu>
2021-07-26 17:57:50 -0500

    Rewrote big and little to use smip_io2

--- big_query.py
+++ big_query.py
@@ -1,15 +1,16 @@
 from datetime import datetime
 from time import perf_counter
 
-import smip_io
+from smip_io2 import SMIP
 
-token = smip_io.get_token("test", "smtamu_group", "parthdave", "parth1234")
-print(token[-6:])
+conn = SMIP("https://smtamu.cesmii.net/graphql", "test",
+            "smtamu_group", "parthdave", "parth1234")
+print(conn.token[-6:])
 print(datetime.now(), 'Starting big download request')
 start_timer = perf_counter()
-r = smip_io.get_data(end_time='2021-07-01T21:22:51.984520+00:00',
-                     start_time='2021-07-01T21:21:51.984520+00:00',
-                     ids=[5356], token=token)
+r = conn.get_data(end_time='2021-07-01T21:22:51.984520+00:00',
+                  start_time='2021-07-01T21:21:51.984520+00:00',
+                  ids=[5356])
 elapsed = perf_counter() - start_timer
 print(len(r.json()['data']['getRawHistoryDataWithSampling']))
 print(datetime.now(), f'Got {len(r.content)} bytes in {elapsed} seconds')
--- little_query.py
+++ little_query.py
@@ -1,42 +1,42 @@
 from datetime import datetime
-from time import perf_counter, sleep
 from math import nan
+from time import perf_counter, sleep
 
-import requests
 import matplotlib.pyplot as plt
+from requests.exceptions import ConnectionError
 
-import smip_io
+from smip_io2 import SMIP
 
-token = smip_io.get_token("test", "smtamu_group", "parthdave", "parth1234")
-print(token[-6:])
+conn = SMIP("https://smtamu.cesmii.net/graphql", "test",
+            "smtamu_group", "parthdave", "parth1234")
+print(conn.token[-6:])
 elapsed_times = list()
 start_times = list()
 try:
-    with requests.Session() as s:
-        while True:
-            start_ts = datetime.now()
-            print(start_ts, 'Starting little download request')
-            start_timer = perf_counter()
-            try:
-                r = smip_io.get_data(end_time='2021-07-01T21:22:53.000000+00:00',
-                                     start_time='2021-07-01T21:22:52.000000+00:00',
-                                     ids=[5356], token=token, session=s)
-            except requests.exceptions.ConnectionError:
-                print(datetime.now(), 'Connection failed')
-                start_times.append(start_ts)
-                elapsed_times.append(nan)
-                continue
-            elapsed = perf_counter() - start_timer
+    while True:
+        start_ts = datetime.now()
+        print(start_ts, 'Starting little download request')
+        start_timer = perf_counter()
+        try:
+            r = conn.get_data(end_time='2021-07-01T21:22:53.000000+00:00',
+                              start_time='2021-07-01T21:22:52.000000+00:00',
+                              ids=[5356])
+        except ConnectionError:
+            print(datetime.now(), 'Connection failed')
             start_times.append(start_ts)
-            elapsed_times.append(elapsed)
-            print(len(r.json()['data']['getRawHistoryDataWithSampling']))
-            print(datetime.now(),
-                  f'Got {len(r.content)} bytes in {elapsed} seconds')
-            sleep(1)
+            elapsed_times.append(nan)
+            continue
+        elapsed = perf_counter() - start_timer
+        start_times.append(start_ts)
+        elapsed_times.append(elapsed)
+        print(len(r.json()['data']['getRawHistoryDataWithSampling']))
+        print(datetime.now(),
+              f'Got {len(r.content)} bytes in {elapsed} seconds')
+        sleep(1)
 except KeyboardInterrupt:
     plt.plot(start_times, elapsed_times, 'o-')
     plt.xlabel('Time started')
     plt.ylabel('Elapsed time (s)')
     plt.title('Little queries elapsed times')
     plt.show()
-    raise
+    exit()
